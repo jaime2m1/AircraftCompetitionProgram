cb31d72a8d4bf23ebcc81c02dab041514e98541a
Jaime <jaime2m1@gmail.com>
2021-04-18 11:58:53 +0200

    Anadido CSS

--- AircraftCompetition/src/Controlador/MainApp.java
+++ AircraftCompetition/src/Controlador/MainApp.java
@@ -2,6 +2,7 @@ package Controlador;
 
 import java.io.IOException;
 
+
 import Modelo.Person;
 import Vista.PersonEditDialogController;
 import Vista.PersonOverviewController;
@@ -10,6 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -101,6 +103,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        
+        this.primaryStage.getIcons().add(new Image("file:resources/images/plane.png"));
 
         initRootLayout();
 
